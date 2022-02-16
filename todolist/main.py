afc40c50df08f5b949b438526953ca670e9f3d2d
Wilhelm Adam <wilhelm.adam@sla.de>
2022-02-16 13:01:05 +0100

    first try on the api

---
+++ todolist/main.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+new_entry = {'id': '1a5eebec-410d-4904-8599-71244fbb25cb',
+             'name': 'Milch einkaufen',
+             'description': '',
+             'user_id': 'e302b6b8-65d0-48b1-a1b3-1e40307ebf51',
+             'list_id': 'c819997b-9b75-44a6-95e1-79da9ed36170'}
+
+list_entries = []
+
+list_entries.append(new_entry)
+
+todo_list = []
+
+todo_list.append("c819997b-9b75-44a6-95e1-79da9ed36170")
+
+
+@app.route('/todo-list/<list_id>', methods=['GET'])
+def getTodolist(list_id):
+    try:
+        todo_list.index(list_id)
+    except ValueError:
+        return "THAT LIST WAS NOT FOUND!"
+    entrys = []
+    for entry in list_entries:
+        if entry["list_id"] == list_id:
+            entrys.append(entry)
+    return entrys.__str__()
+
+
+
+@app.route('/todo-list/<list_id>', methods=['DELETE'])
+def deleteTodolist(list_id):
+    return f'your trying to delete the todo-list {list_id}'
+
+
+@app.route('/todo-list', methods=['POST'])
+def createTodolist():
+    return f'your trying to create a todo-list'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=1)
